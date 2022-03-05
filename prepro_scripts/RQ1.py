0cd7ab0300bf95698249fb2de36ca85045d5b9ea
Antonio Schettino <schettino@eur.nl>
2022-03-05 11:15:03 +0100

    slightly cleaner script

--- prepro_scripts/RQ1.py
+++ prepro_scripts/RQ1.py
@@ -14,11 +14,13 @@ install libraries
 import libraries
 '''
 
-import mne
-# import pandas as pd
-# import scipy.io
 import os
 import numpy as np
+import mne
+import random
+
+# import pandas as pd
+# import scipy.io
 # from scipy.io import loadmat  # this is the SciPy module that loads mat-files
 # import matplotlib.pyplot as plt
 # from datetime import datetime, date, time
@@ -37,6 +39,9 @@ import numpy as np
 #         if not os.path.isdir(ed):
 #             raise
 
+project_seed = 999 # RNG seed
+random.seed(project_seed) # set seed to ensure computational reproducibility
+
 '''
 test procedure on single participant
 '''
@@ -52,11 +57,8 @@ preproc_path = '/home/aschetti/Documents/Projects/code_mechanics/eeg_BIDS/'
 fname_epochs = os.path.join(preproc_path, 'sub-001-epo-ERP_RQ1_autoreject_0.1_HP_.fif')
 epochs = mne.read_epochs(fname_epochs, preload = True)
 
-# show number of epochs per condition
-print(epochs)
-
 '''
-baseline correction & epoch selection
+baseline correction
 '''
 
 # baseline correction
@@ -64,108 +66,142 @@ baseline correction & epoch selection
 ####### TO DECIDE #######
 
 
-epochs, dropped_epochs = epochs.equalize_event_counts() # drops 40 epochs from 'natural' condition, to have equal epoch number as `manmade`
+'''
+channel & epoch selection
+'''
+
+channels = mne.pick_types(epochs.info, eeg = True, exclude = ['M1', 'M2', 'VEOG', 'HEOG']) # pick scalp electrodes only
+
+print(epochs) # show number of epochs per condition
+
+epochs, dropped_epochs = epochs.equalize_event_counts() # ensure equal epoch number across conditions
+
+epochs_data = epochs.get_data(picks = channels) # select data from scalp electrodes only
+
+'''
+trial-averaged evoked potentials
+'''
+
+all_evoked = epochs.average() # all conditions
+manmade_evoked = epochs['manmade'].average() # `manmade`
+natural_evoked = epochs['natural'].average() # 'natural'
+diff_manmade_natural_evoked = mne.combine_evoked([manmade_evoked, natural_evoked], weights = [1, -1]) # difference between conditions
 
 '''
 visualization
 '''
 
-### visualize epochs
+topo_times = [0, 0.08, 0.125, 0.18]
 
-# evokeds = dict(manmade = list(epochs['manmade'].iter_evoked()),
-               # natural = list(epochs['natural'].iter_evoked()))
-
-# picks = ['P9', 'P7', 'P5', 'P3', 'P1', 'Pz', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO7', 'PO3', 'O1', 'POz', 'Oz', 'Iz', 'PO4', 'PO8', 'O2'] # channels
-# mne.viz.plot_compare_evokeds(evokeds, combine = 'mean', picks = picks)
-
-# select epochs
-manmade_epochs = epochs['manmade'] # `manmade` epochs only
-natural_epochs = epochs['natural'] # 'natural' epochs only
-
-# trial-averaged evoked potentials
-manmade_evoked = manmade_epochs.average() # `manmade`
-natural_evoked = natural_epochs.average() # 'natural'
-
-# Global Field Power
-# mne.viz.plot_compare_evokeds(dict(manmade = manmade_evoked, natural = natural_evoked),
-#                              picks = 'eeg',
-#                              legend='upper left', show_sensors='lower right')
-
-## inspect trial-averaged epochs
-# # butterfly plots
-# butterfly_manmade_evoked = manmade_evoked.plot(spatial_colors = True) # `manmade`
-# butterfly_natural_evoked = natural_evoked.plot(spatial_colors = True) # `natural`
-# # topographies
-# topomap_manmade_evoked = manmade_evoked.plot_topomap(title = 'manmade', times = np.arange(0, 0.55, 0.05)) # `manmade`
-# topomap_natural_evoked = natural_evoked.plot_topomap(title = 'natural', times = np.arange(0, 0.55, 0.05)) # `natural`
-# joint butterfly + topographies
-butterfly_topomap_manmade_evoked = manmade_evoked.plot_joint(title = 'manmade', times = [0, 0.08, 0.125, 0.18]) # `manmade`
-butterfly_topomap_natural_evoked = natural_evoked.plot_joint(title = 'natural', times = [0, 0.08, 0.125, 0.18]) # `natural`
-
-# trial-averaged epochs
-all_evoked = epochs.average() # all conditions
-# joint butterfly + topographies
-butterfly_topomap_all_evoked = all_evoked.plot_joint(title = 'all epochs', times = [0, 0.08, 0.125, 0.18]) # all conditions
-
-# trial-averaged difference between conditions
-evoked_diff = mne.combine_evoked([manmade_evoked, natural_evoked], weights = [1, -1])
-butterfly_topomap_evoked_diff = evoked_diff.plot_joint(title = 'condition difference', times = [0, 0.08, 0.125, 0.18]) # all conditions
+butterfly_topomap_all_evoked = all_evoked.plot_joint(title = 'all epochs', times = topo_times) # all conditions
+butterfly_topomap_manmade_evoked = manmade_evoked.plot_joint(title = 'manmade', times = topo_times) # `manmade`
+butterfly_topomap_natural_evoked = natural_evoked.plot_joint(title = 'natural', times = topo_times) # `natural`
+butterfly_topomap_diff_manmade_natural_evoked = diff_manmade_natural_evoked.plot_joint(title = 'condition difference', times = topo_times) # all conditions
 
 '''
 analysis
 '''
 
-times = epochs.times # extract time samples
-all_data = epochs.get_data() # extract data
+# show electrode montage
+epochs.plot_sensors(ch_type = 'eeg', show_names = True)
 
-time_window = np.logical_and(0 <= times, times <= 0.3) # time window (0-300 ms --> 0-0.3 sec)
-data_permute = np.mean(all_data[:, :, time_window], axis = 2) # signal in specified time window
+# define time window
+t_min = 0
+t_max = 0.3
 
-n_perm = 1000 # number of permutations (should be at least 1000)
-
-T0, p_values, H0 = mne.stats.permutation_t_test(data_permute, n_perm, n_jobs = 1)
-
-
-###### TO DO ######
-# select electrodes in data_permute based on the labels in epochs.info
-# goal: only 
-
-
-
-
-tempor = arr_a[arr_a == arr_b]
+channels = mne.pick_types(epochs.info, eeg = True, exclude = ['M1', 'M2', 'VEOG', 'HEOG']) # pick scalp electrodes only
 
 
 
 
 
+all_data = epochs.get_data(picks = channels, tmin = t_min, tmax = t_max) # extract data from selected electrodes and time window
+all_data_permute = np.mean(all_data, axis = 2) # trial-averaged signal
+
+# np.shape(all_data_permute)
+
+# TO DELETE
+# all_data = epochs.get_data()
+# time_window = np.logical_and(0 <= times, times <= 0.3) # time window (0-300 ms --> 0-0.3 sec)
+# data_permute = np.mean(all_data[:, :, time_window], axis = 2) # signal in specified time window
+
+n_perm = 1000 # number of permutations (should be at least 1000
+
+# T_obs, p_values, H0 = mne.stats.permutation_t_test(all_data_permute, n_perm, n_jobs = 1)
 
 
+T_obs, p_values, H0 = mne.stats.permutation_t_test(all_data_permute, n_permutations = n_perm, tail = -1, n_jobs = 1, seed = project_seed)
 
-arr_a = np.array(['1m_nd', '2m_nd', '1m_4wk'], dtype='<U15')
-arr_b =  np.array('1m_nd')
-tempor = arr_a[arr_a == arr_b]
-
-
-
-picks = mne.pick_types(epochs.info, eeg = True, exclude = '')
-
-
-
-
-picks = mne.pick_types(epochs.info, eeg = True, exclude = 'bads')
-
-significant_electrodes = picks[p_values <= 0.05]
+significant_electrodes = channels[p_values <= 0.05]
 significant_electrode_names = [epochs.ch_names[k] for k in significant_electrodes]
 
 print("Number of significant electrodes : %d" % len(significant_electrodes))
 print("Electrode labels : %s" % significant_electrode_names)
 
 
+# np.shape(T_obs)
+# np.shape(p_values)
+# np.shape(H0)
+
+
+manmade_evoked
+
+
+# Transform each condition to array
+manmade_epochs = np.mean(epochs['manmade'].get_data(), axis = 1)
+natural_epochs = np.mean(epochs['natural'].get_data(), axis = 1)
+
+# Permutation test to find significant cluster of differences
+T_obs, p_values, H0 = mne.stats.permutation_t_test([manmade_epochs, natural_epochs], seed = project_seed)
 
 
 
 
+
+
+
+
+
+
+
+
+# topoplots with highlighted electrodes
+# times = (0.09, 0.1, 0.11)
+# _times = ((np.abs(evoked.times - t)).argmin() for t in times)
+# significant_channels = [
+#     ('MEG 0231', 'MEG 1611', 'MEG 1621', 'MEG 1631', 'MEG 1811'),
+#     ('MEG 2411', 'MEG 2421'),
+#     ('MEG 1621')]
+# _channels = [np.in1d(evoked.ch_names, ch) for ch in significant_channels]
+
+# mask = np.zeros(evoked.data.shape, dtype='bool')
+# for _chs, _time in zip(_channels, _times):
+#     mask[_chs, _time] = True
+
+# evoked.plot_topomap(times, ch_type='mag', time_unit='s', mask=mask,
+#                     mask_params=dict(markersize=10, markerfacecolor='y'))
+
+
+
+ # for our spatio-temporal clustering functions, the spatial dimensions need to be last 
+ # for computational efficiency reasons. For example,
+ # for mne.stats.spatio_temporal_cluster_1samp_test(), X
+ # needs to be of shape (n_samples, n_time, n_space). 
+ # You can use numpy.transpose() to transpose axes if necessary.
+
+
+
+evoked = mne.EvokedArray(-np.log10(p_values)[:, np.newaxis], epochs.info, tmin = 0.)
+
+# Extract mask and indices of active sensors in the layout
+stats_picks = mne.pick_channels(evoked.ch_names, significant_sensors_names)
+mask = p_values[:, np.newaxis] <= 0.05
+
+evoked.plot_topomap(ch_type='grad', times=[0], scalings=1,
+                    time_format=None, cmap='Reds', vmin=0., vmax=np.max,
+                    units='-log10(p)', cbar_fmt='-%0.1f', mask=mask,
+                    size=3, show_names=lambda x: x[4:] + ' ' * 20,
+                    time_unit='s')
 
 
 
@@ -183,7 +219,20 @@ mne.viz.plot_compare_evokeds(evokeds, combine = 'mean', picks = picks)
 
 
 
+# # topoplots with highlighted electrodes
+# times = (0.09, 0.1, 0.11)
+# _times = ((np.abs(evoked.times - t)).argmin() for t in times)
+# significant_channels = [
+#     ('MEG 0231', 'MEG 1611', 'MEG 1621', 'MEG 1631', 'MEG 1811'),
+#     ('MEG 2411', 'MEG 2421'),
+#     ('MEG 1621')]
+# _channels = [np.in1d(evoked.ch_names, ch) for ch in significant_channels]
 
+# mask = np.zeros(evoked.data.shape, dtype='bool')
+# for _chs, _time in zip(_channels, _times):
+#     mask[_chs, _time] = True
 
+# evoked.plot_topomap(times, ch_type='mag', time_unit='s', mask=mask,
+#                     mask_params=dict(markersize=10, markerfacecolor='y'))
 
 
