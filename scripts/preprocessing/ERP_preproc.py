c762c63f5a17ff1f9afd3b52a5c9c970a32d3820
Antonio Schettino <schettino@eur.nl>
2022-03-11 19:42:50 +0100

    load raw data

--- scripts/preprocessing/ERP_preproc.py
+++ scripts/preprocessing/ERP_preproc.py
@@ -1,66 +1,173 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
+'''
+Preprocessing of raw data for ERP analysis 
 
-This is a temporary script file.
-"""
+sources: 
+    https://neuraldatascience.io/7-eeg/erp_group.html
+    https://mne.tools/stable/auto_tutorials/evoked/30_eeg_erp.html#sphx-glr-auto-tutorials-evoked-30-eeg-erp-py
+    https://mne.tools/stable/auto_tutorials/stats-sensor-space/20_erp_stats.html#sphx-glr-auto-tutorials-stats-sensor-space-20-erp-stats-py
 
-import mne
-import pandas as pd
-import scipy.io
-import os
+@author: Antonio Schettino
+'''
+
+'''
+import libraries
+'''
+
+import random
 import numpy as np
-from scipy.io import loadmat  # this is the SciPy module that loads mat-files
-import matplotlib.pyplot as plt
-from datetime import datetime, date, time
+# import glob
+# import re
+# import matplotlib.pyplot as plt
+import os
+import mne
+# import time
+
 from os.path import join as opj
-from glob import glob
-from pyprep.prep_pipeline import PrepPipeline
-import time
-from mne_bids import BIDSPath, read_raw_bids, print_dir_tree, make_report
+from mne_bids import BIDSPath, read_raw_bids
+# from mne_bids import BIDSPath, read_raw_bids, print_dir_tree, make_report
+# from pyprep import NoisyChannels
+# from mne.preprocessing import ICA, create_eog_epochs, create_ecg_epochs, corrmap
+# from autoreject import AutoReject
 
-from pyprep import NoisyChannels
-from mne.preprocessing import (ICA, create_eog_epochs, create_ecg_epochs,
-                               corrmap)
-from autoreject import AutoReject
-import seaborn as sns
-def ensure_dir(ed):
-    import os
-    try:
-        os.makedirs(ed)
-    except OSError:
-        if not os.path.isdir(ed):
-            raise
+'''
+setup
+'''
 
+project_seed = 999 # RNG seed
+random.seed(project_seed) # set seed to ensure computational reproducibility
 
+raw_path = '/home/aschetti/Documents/Projects/code_mechanics/data/raw/eeg_BIDS/' # directory with raw data
+# preproc_path = '/home/aschetti/Documents/Projects/code_mechanics/eeg_BIDS/' # directory with preprocessed files
 
+# filenames = glob.glob(preproc_path + '/*.fif') # list of .fif files in directory
 
-# starting from a relative path /eeg_BIDS which you should also have
-
-bids_root = '../eeg_BIDS/'
+conditions = ['manmade', 'natural'] # condition names
 
 datatype = 'eeg'
-suffix = 'eeg'
-# get all subject names
 
-subs=[ name for name in os.listdir(bids_root) if name.startswith('sub') ]
+# # plot parameters
+# sub_pattern = re.compile(r"\bsub-0\w*-\b") # regex pattern for plot titles 
 
+# filter cutoffs
+cutoff_l = 0.1
+cutoff_h = 100
+# cutoff_h = None # we also get rid of anything higher than 100Hz which is typically not of relevance to human studies
 
-# This needs to be changed for ERP analyses!!! This is now for TFR
-cutoff_l=1 # the low cutoff is 1Hz to get rid of the slow drifts
-cutoff_h=None # we also get rid of anything higher than 100Hz which is typically not of relevance to human studies
-
-
-
-# set the referencing method
+# referencing method
 reference = 'average'
 
-for subject in subs[:1]:
+'''
+preprocessing
+'''
+
+# get all subject numbers
+subs = [ name for name in os.listdir(raw_path) if name.startswith('sub') ] 
+
+for ssj in subs[:1]:
     
-    print(subject)
+    print(ssj)
     
-    bids_path = BIDSPath(subject=subject[-3:], task ='xxxx',suffix= suffix,
-                          datatype=datatype, root=bids_root)
+    # create BIDS path
+    bids_path = BIDSPath(
+        subject = ssj[-3:], # subject identifier (remove 'sub-' because '-' is not allowed as character)
+        session = None, # acquisition session: not specified
+        task = 'xxxx', # task name
+        acquisition = None, # acquisition parameters: not specified
+        run = None, # run number: not specified
+        processing = None, # processing label: not specified
+        recording = None, # recording name: not specified
+        space = None, # coordinate space (for anatomical and sensor location): not specified
+        split = None, # split of continuous recording file: not specified
+        suffix = datatype, # filename suffix
+        extension = None, # file extension: not specified
+        datatype = datatype, # BIDS data type        
+        root = raw_path, # directory of BIDS dataset
+        check = True # BIDS conformity
+        )
+    
+    # load data
+    raw = read_raw_bids(bids_path)
+    # .pick_types(eeg = True, 
+    #                      stim = True, 
+    #                      eog = True, 
+    #                      misc = True, 
+    #                      # include = (), 
+    #                      exclude = 'bads'
+    #                      # selection = None,
+    #                      # verbose = None
+    #                      )
+    
+    print(raw.info)
+    
+    
+    n_time_samps = raw.n_times
+    time_secs = raw.times
+    ch_names = raw.ch_names
+    n_chan = len(ch_names)  # note: there is no raw.n_channels attribute
+    print('the (cropped) sample data object has {} time samples and {} channels.'
+      ''.format(n_time_samps, n_chan))
+    print('The last time sample is at {} seconds.'.format(time_secs[-1]))
+    print('The first few channel names are {}.'.format(', '.join(ch_names[:3])))
+    print()  # insert a blank line in the output
+    # some examples of raw.info:
+    print('bad channels:', raw.info['bads'])  # chs marked "bad" during acquisition
+    print(raw.info['sfreq'], 'Hz')            # sampling frequency
+    print(raw.info['description'], '\n')      # miscellaneous acquisition info
+    print(raw.info)
+    
+    
+    
+    
+    
+    # select channels
+    raw = raw.pick_types(eeg = True, 
+                         stim = True, 
+                         eog = True, 
+                         misc = True, 
+                         # include = (), 
+                         exclude = 'bads'
+                         # selection = None,
+                         # verbose = None
+    )
+    
+
+    
+    
+    
+    # load raw data
+    raw = mne.io.read_raw_brainvision(
+        bids_path, 
+        eog = ('VEOG', 'HEOG','IO1','IO2','Afp9','Afp10'),
+        misc = ('M1','M2'), 
+        scale = 1.0, 
+        preload = True, 
+        verbose = False
+        )
+
+    
+    
+    
+    
+    # downsample
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    bids_path = BIDSPath(subject = subject[-3:], task = 'xxxx',suffix = suffix,
+                          datatype = datatype, root = raw_path)
+    
     raw = mne.io.read_raw_brainvision(bids_path, eog=('VEOG', 'HEOG','IO1','IO2','Afp9','Afp10'), misc=('M1','M2'), scale=1.0, preload=True, verbose=False)
 
 
