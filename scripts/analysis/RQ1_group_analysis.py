5b68d89f2de171d6684e8e9433379ba4f7cd0981
Antonio Schettino <schettino@eur.nl>
2022-03-18 10:16:22 +0100

    use Ana's folder structure

--- scripts/analysis/RQ1_group_analysis.py
+++ scripts/analysis/RQ1_group_analysis.py
@@ -32,18 +32,13 @@ project_seed = 999 # RNG seed
 random.seed(project_seed) # set seed to ensure computational reproducibility
 
 # directories
-# raw_path = '/home/aschetti/Documents/Projects/code_mechanics/data/raw/eeg_BIDS/' # directory with raw data
-preproc_path = '/home/aschetti/Documents/Projects/code_mechanics/data/preproc/' # directory with preprocessed files
-# events_path = '/home/aschetti/Documents/Projects/code_mechanics/data/events/' # directory with event files
+preproc_path = '/home/aschetti/Documents/Projects/code_mechanics/data/processed_data/ERP/' # directory with preprocessed files
 
 datatype = 'eeg' # data type
 
 # define electrode montage
 montage = mne.channels.make_standard_montage("biosemi64")
 # montage.plot() # visualize montage
-
-# get all participant names
-subs = [name for name in os.listdir(preproc_path) if name.startswith('sub')] 
 
 # filenames = glob.glob(preproc_path + '/*.fif') # list of .fif files in directory
 
@@ -67,7 +62,14 @@ t_max = 0.3
 tfce_params = dict(start = .2, step = .2) # parameter values
 n_perm = 5000 # number of permutations (at least 1000)
 
+# plot parameters
+time_unit = dict(time_unit = "s") # time unit
+
 # %% data cleaning (one dataset)
+
+# get all participant names
+subs = [name for name in os.listdir(preproc_path) if name.startswith('sub')] 
+
 
 for ssj in subs:
 
@@ -92,7 +94,7 @@ for ssj in subs:
             )
     
     # %% create condition-specific evoked responses
-            
+    
     # evoked responses (weighted average)
     evoked_manmade = epochs_manmade.average() # 'manmade' condition
     evoked_natural = epochs_natural.average() # 'natural' condition
@@ -126,10 +128,10 @@ for ssj in subs:
     # grand average (weighted average)
     evoked_grand_average = mne.combine_evoked([evoked_manmade, evoked_natural], weights = 'nave')
    
-    # joint plots (butterfly + topography)
+    # joint plot (butterfly + topography)
     evoked_grand_average.plot_joint(times = topo_times, title = opj(ssj + '_localizer')) # collapsed localizer
     
-    # show localizer
+    # localizer ERP
     mne.viz.plot_compare_evokeds(evoked_grand_average, 
                                  combine = 'mean',
                                  legend = None,
@@ -183,7 +185,7 @@ for ssj in subs:
     
     
     
-    # artificially create epochs of collapsed localizer 
+    # artificially create epochs of localizer 
     localizer_epochs = mne.EpochsArray(
         localizer_data, 
         mne.create_info( # create info structure
@@ -203,24 +205,15 @@ for ssj in subs:
     localizer_evoked.plot_image(
         mask = significant_time_points, 
         show_names = "all", 
-        titles =  opj(ssj + '_collapsed_localizer')
-        )
-
-
-
-
-
-
-
-
-    time_unit = dict(time_unit = "s")
+        titles =  opj(ssj + '_localizer')
+        )   
     
-    
+    # joint plot (butterfly + topography)
     localizer_evoked.plot_joint(        
         times = topo_times,
         ts_args = time_unit,
         topomap_args = time_unit,
-        title = opj(ssj + '_collapsed_localizer')        
+        title = opj(ssj + '_localizer')        
         )
     
     
@@ -229,19 +222,74 @@ for ssj in subs:
     
     
     
-    # Create ROIs by checking channel labels
-    selections = make_1020_channel_selections(evoked.info, midline="12z")
+    # Visualize
+    ## <string>:1: RuntimeWarning: Ignoring argument "tail", performing 1-tailed F-test
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, ncols=1, sharex=True)
     
-    # Visualize the results
-    fig, axes = plt.subplots(nrows=3, figsize=(8, 8))
-    axes = {sel: ax for sel, ax in zip(selections, axes.ravel())}
-    evoked.plot_image(axes=axes, group_by=selections, colorbar=False, show=False,
-                      mask=significant_points, show_names="all", titles=None,
-                      **time_unit)
-    plt.colorbar(axes["Left"].images[-1], ax=list(axes.values()), shrink=.3,
-                 label="µV")
+    times = epochs.times
+    ax0.axvline(x=0, linestyle="--", color="black")
+    ## <matplotlib.lines.Line2D object at 0x00000000BFAE7640>
+    ax0.plot(times, np.mean(condition1, axis=0), label="Audio")
+    ## [<matplotlib.lines.Line2D object at 0x00000000BFAF3D30>]
+    ax0.plot(times, np.mean(condition2, axis=0), label="Visual")
+    ## [<matplotlib.lines.Line2D object at 0x00000000BFAF3F40>]
+    ax0.legend(loc="upper right")
+    ## <matplotlib.legend.Legend object at 0x00000000BFAF3EE0>
+    ax0.set_ylabel("uV")
     
-    plt.show()
+    # Difference
+    ax1.axvline(x=0, linestyle="--", color="black")
+    ax1.plot(times, condition1.mean(axis=0) - condition2.mean(axis=0))
+    ## [<matplotlib.lines.Line2D object at 0x00000000BFB07B20>]
+    ax1.axhline(y=0, linestyle="--", color="black")
+    ## <matplotlib.lines.Line2D object at 0x00000000BFB07CD0>
+    ax1.set_ylabel("Difference")
+    
+    # T-values
+    ax2.axvline(x=0, linestyle="--", color="black")
+    h = None
+    for i, c in enumerate(clusters):
+        c = c[0]
+        if p_vals[i] <= 0.05:
+            h = ax2.axvspan(times[c.start],
+                            times[c.stop - 1],
+                            color='red',
+                            alpha=0.5)
+        else:
+            ax2.axvspan(times[c.start],
+                        times[c.stop - 1],
+                        color=(0.3, 0.3, 0.3),
+                        alpha=0.3)
+                ## <matplotlib.patches.Polygon object at 0x00000000BFB16550>
+                ## <matplotlib.patches.Polygon object at 0x00000000BFB16BE0>
+        hf = ax2.plot(times, t_vals, 'g')
+        if h is not None:
+            plt.legend((h, ), ('cluster p-value < 0.05', ))
+                    ## <matplotlib.legend.Legend object at 0x00000000BFB16D00>
+            plt.xlabel("time (ms)")
+                    ## Text(0.5, 0, 'time (ms)')
+            plt.ylabel("t-values")
+                    ## Text(0, 0.5, 't-values')
+            plt.savefig("figures/fig2.png")
+            plt.clf()
+                    
+                    
+    
+    
+    
+    # # Create ROIs by checking channel labels
+    # selections = make_1020_channel_selections(evoked.info, midline="12z")
+    
+    # # Visualize the results
+    # fig, axes = plt.subplots(nrows=3, figsize=(8, 8))
+    # axes = {sel: ax for sel, ax in zip(selections, axes.ravel())}
+    # evoked.plot_image(axes=axes, group_by=selections, colorbar=False, show=False,
+    #                   mask=significant_points, show_names="all", titles=None,
+    #                   **time_unit)
+    # plt.colorbar(axes["Left"].images[-1], ax=list(axes.values()), shrink=.3,
+    #              label="µV")
+    
+    # plt.show()
 
 
 
@@ -459,7 +507,9 @@ plt.clf()
 
 
 
-
+'''
+old code
+'''
 
 # preallocate arrays
 epochs = {} # epochs
@@ -577,175 +627,5 @@ for i in range(len(filenames)): # loop through files
 
 
 
-
-
-
-
-
-'''
-old code
-'''  
-
-
-
-'''
-load preprocessed epoched data
-'''
-
-epochs = {} # preallocate array
-
-for i in range(len(filenames)): # loop through files
-    epochs[i] = mne.read_epochs(filenames[i], preload = False) # load data (preload  = False or python will crash on my coomputer)
-
-'''
-equalize epochs across conditions
-'''
-
-epochs_equal = {} # store equalized epochs
-dropped_epochs = {} # dropped epochs per dataset
-
-for i in range(len(filenames)):
-    epochs_equal[i], dropped_epochs[i] = epochs[i].equalize_event_counts() # drop epochs from condition with more data
-    
-# WE LOSE A LOT OF DATA! Double-check if this is still necessary after fixing the preprocessing pipeline!
-
-'''
-create trial-averaged evoked data
-'''
-
-evoked_equal = {} # store trial-averaged evoked data
-
-for i in range(len(filenames)):
-    evoked_equal[i] = {c:epochs_equal[i][c].average() for c in conditions}
-         
-'''
-plots
-'''  
-
-
-
-
-####### trial-averaged epochs #######
-evoked_all = {} # collapsed across conditions
-evoked_manmade = {} # manmade condition
-evoked_natural = {} # natural condition
-evoked_diff_manmade_natural = {} # difference between conditions
-
-for j in range(len(epochs)):   
-    evoked_all[j] = epochs_equal[j].average() # collapsed across conditions
-    evoked_manmade[j] = epochs_equal[j]['manmade'].average() # manmade condition
-    evoked_natural[j] = epochs_equal[j]['natural'].average() # natural condition
-    evoked_diff_manmade_natural[j] = mne.combine_evoked([evoked_manmade[j], evoked_natural[j]], weights = [1, -1]) # difference between conditions
-
-#################################################   
-
-####### extract data from EEG channels only #######
-evoked_data_all = {} # collapsed across conditions
-evoked_data_manmade = {} # manmade condition
-evoked_data_natural = {} # natural condition
-evoked_data_diff_manmade_natural = {} # difference between conditions
-
-for m in range(len(epochs)):
-    evoked_data_all[m] = evoked_all[m].get_data(picks = channels)
-    evoked_data_manmade[m] = evoked_manmade[m].get_data(picks = channels)
-    evoked_data_natural[m] = evoked_natural[m].get_data(picks = channels)
-    evoked_data_diff_manmade_natural[m] = evoked_diff_manmade_natural[m].get_data(picks = channels)
-
-#################################################
-
-# ####### grand-averages (collapsed across participants) #######
-
-# list0 = evoked_data_all[0]
-# list1 = evoked_data_all[1]
-# list2 = evoked_data_all[2]
-# list3 = evoked_data_all[3]
-# list4 = evoked_data_all[4]
-# list5 = evoked_data_all[5]
-# list6 = evoked_data_all[6]
-# list7 = evoked_data_all[7]
-# list8 = evoked_data_all[8]
-# list9 = evoked_data_all[9]
-# list10 = evoked_data_all[10]
-# list11 = evoked_data_all[11]
-# list12 = evoked_data_all[12]
-# list13 = evoked_data_all[13]
-# list14 = evoked_data_all[14]
-# list15 = evoked_data_all[15]
-# list16 = evoked_data_all[16]
-# list17 = evoked_data_all[17]
-# list18 = evoked_data_all[18]
-# list19 = evoked_data_all[19]
-# list20 = evoked_data_all[20]
-# list21 = evoked_data_all[21]
-# list22 = evoked_data_all[22]
-# list23 = evoked_data_all[23]
-# list24 = evoked_data_all[24]
-# list25 = evoked_data_all[25]
-# list26 = evoked_data_all[26]
-# list27 = evoked_data_all[27]
-# list28 = evoked_data_all[28]
-# list29 = evoked_data_all[29]
-# list30 = evoked_data_all[30]
-# list31 = evoked_data_all[31]
-# list32 = evoked_data_all[32]
-
-# product = np.multiply(list0, list1, list2, list3, list4, list5, list6, list7, list8, list9, list10, list11, list12, list13, list14, list15, list16, list17, list18, list19, list20, list21, list22, list23, list24, list25, list26, list27, list28, list29, list30, list31, list32)
-
-# print(product)
-
-# grandavg_all = {} # collapsed across conditions
-# grandavg_manmade = {} # manmade condition
-# grandavg_natural = {} # natural condition
-
-# for n in range(len(epochs)):
-#     grandavg_all[n] = np.mean(evoked_data_all[n], axis = 1) #
-#     grandavg_manmade[n] = np.mean(epochs_equal_data
-#     grandavg_natural[n] = np.mean(
-   
-# #################################################
-   
-'''
-visualization
-'''
-
-
-
-
-
-butterfly_topomap_all_evoked = all_evoked.plot_joint(title = 'all epochs', times = topo_times) # all conditions
-
-butterfly_topomap_evoked_manmade = evoked_manmade.plot_joint(title = 'manmade', times = topo_times) # `manmade`
-butterfly_topomap_evoked_natural = evoked_natural.plot_joint(title = 'natural', times = topo_times) # `natural`
-
-butterfly_topomap_diff_manmade_natural_evoked = diff_manmade_natural_evoked.plot_joint(title = 'condition difference', times = topo_times) # difference between conditions
-
-
-    
-    
-
-    
-'''
-compare evoked waveforms
-'''
-
-
-    
-    
-    
-
-channels = [mne.pick_types(epochs, eeg = True, exclude = ['M1', 'M2', 'VEOG', 'HEOG']) for f in filenames] # pick scalp electrodes only
-
-
-
-
-
-# # ensure equal epoch number across conditions
-# epochs, dropped_epochs = [epochs.equalize_event_counts(conditions) for f in filenames]
-
-
-epochs, dropped_epochs = [epochs.equalize_event_counts(conditions) for f in filenames]
-
-
-epochs_data = epochs.get_data(picks = channels) # select data from scalp electrodes only
 
 
