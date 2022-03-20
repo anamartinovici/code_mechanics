18e31273249cffa4d584f334efaf78f3787cdc35
Antonio Schettino <schettino@eur.nl>
2022-03-20 11:52:34 +0100

    ggplot theme

--- scripts/analysis/functions/custom_ggplot_theme.R
+++ scripts/analysis/functions/custom_ggplot_theme.R
@@ -1,12 +1,12 @@
-theme_C1 <-
-  theme_minimal(base_size = 16) +
+theme_custom <-
+  theme_minimal(base_size = 20) +
   theme(
     strip.text = element_text(
       hjust = .5,
       size = 20
     ),
     plot.background = element_rect(fill = "white", color = "transparent"),
-    plot.title = element_text(size = 26, hjust = .5),
+    plot.title = element_text(size = 28, hjust = .5, face = "bold"),
     legend.box.background = element_rect(color = "transparent"),
     legend.position = "none"
   )
