3b7730d8ff391fd610c2732427caa794b8dc7786
Antonio Schettino <schettino@eur.nl>
2022-03-21 20:33:37 +0100

    N1 parameter estimation

--- scripts/analysis/RQ1/02_parameter_estimation.R
+++ scripts/analysis/RQ1/02_parameter_estimation.R
@@ -6,24 +6,41 @@ set.seed(project_seed) # set seed
 
 # install packages --------------------------------------------------------------------
 
-# install.packages("Rmisc")
 # install.packages("here")
 # install.packages("tidyverse")
 # install.packages("brms")
 
 # load packages --------------------------------------------------------------------
 
-library(Rmisc) # must be loaded before tidyverse (beware some nasty function masking!)
 library(here)
 library(tidyverse)
 library(brms)
 
-# setup --------------------------------------------------------------------
+# set directories --------------------------------------------------------------------
 
-# electrodes in ROI
-ROI <- c('PO7', 'PO3', 'O1', 'PO4', 'PO8', 'O2', 'POz', 'Oz', 'Iz')
+# N1 data
+N1_path <- here("data", "processed_data", "ERP", "RData", "RQ1")
 
-# setup STAN
+# model fit
+model_path <- here("data", "processed_data", "ERP", "models", "RQ1")
+# create directory if it doesn't exist
+if (dir.exists(model_path)) {
+  print(paste0("The directory '", model_path, "' already exists."))
+} else {
+  dir.create(path = model_path)
+  print(paste0("Directory '", model_path, "' created."))
+}
+
+# setup: N1 --------------------------------------------------------------------
+
+# electrode ROI (region of interest)
+ROI <- c("PO7", "PO3", "O1", "PO4", "PO8", "O2")
+
+# time window for mean N1
+time_window <- c(130, 180)
+
+# setup: STAN --------------------------------------------------------------------
+
 num_chains <- 8 # number of chains = number of processor cores
 num_iter <- 2000 # number of samples per chain
 num_warmup <- 1000 # number of warm-up samples per chain
@@ -31,75 +48,23 @@ num_thin <- 1 # thinning: extract one out of x samples per chain
 
 # priors  --------------------------------------------------------------------
 
-# informative priors
+# informative priors (based on grand average)
 priors <- c(
-  prior("normal(-4, 3)", class = "b", coef = "Intercept"),
+  prior("normal(4, 2)", class = "b", coef = "Intercept"), 
   prior("normal(0, 3)", class = "b"),
-  set_prior("student_t(3, 0, 2)", class = "sd")
+  prior("student_t(3, 0, 2)", class = "sd")
 )
 
-# load and prepare data  --------------------------------------------------------------------
+# load data  --------------------------------------------------------------------
 
-# list of .RData files in directory
-list_RData <-
-  list.files(
-    path = here("data", "processed_data", "ERP", "RData"),
-    pattern = "_ERP.RData"
-  )
-
-# preallocate data frame with all N1 data
-all_N1 <- NULL
-
-# yes, I know I shouldn't use loops in R
-for (i in list_RData) {
-  
-  # load .RData
-  load(here("data", "processed_data", "ERP", "RData", i))
-
-  # extract N1 amplitude from selected ROI and time window
-  N1 <-
-    ERP %>%
-    # create new column
-    mutate(
-      condition = case_when( # manmade/natural conditions
-        manmade == 1 ~ "manmade",
-        natural == 1 ~ "natural"
-      )
-    ) %>%
-    select(ssj, time, epoch_num, condition, all_of(ROI)) %>% # keep only columns of interest
-    filter(time >= 100 & time <= 150) %>% # keep only data in selected time window
-    pivot_longer(
-      !c(ssj, time, epoch_num, condition), # keep as columns
-      names_to = "electrode",
-      values_to = "amplitude"
-    ) %>% 
-    summarySEwithin(
-      data = .,
-      measurevar = "amplitude",
-      withinvars = c("epoch_num", "condition"),
-      idvar = "ssj",
-      na.rm = FALSE,
-      conf.interval = .95
-    ) %>% 
-    add_column(
-      ssj = sub("_.*", "", i), # add column with participant number
-      .before = "epoch_num"
-    ) %>% 
-    select(-c(N, sd, se, ci)) %>% # remove unnecessary columns
-    na.omit # drop NAs
-    
-  # all N1 data
-  all_N1 <- rbind(all_N1, N1)
-  
-}
+load(here(N1_path, "RQ1_all_N1.RData"))
 
 # sampling  --------------------------------------------------------------------
 
-# model
 N1_brms <-
   brm(
-    amplitude ~ 0 + Intercept + condition + (1 + condition | ssj),
-    # amplitude ~ 0 + Intercept + condition + (1 + condition | ssj) + (1 + condition  | epoch_num),
+    amplitude ~ 0 + Intercept + condition_RQ1 + (1 + condition_RQ1 | ssj),
+    # amplitude ~ 0 + Intercept + condition_RQ1 + (1 + condition_RQ1 | ssj) + (1 + condition_RQ1  | epoch_num),
     data = all_N1,
     family = gaussian(),
     prior = priors,
@@ -115,13 +80,8 @@ N1_brms <-
     algorithm = "sampling",
     cores = num_chains,
     seed = project_seed,
-    file = here("data", "processed_data", "ERP", "models", "N1_brms.rds")
+    file = here(model_path, "N1_brms.rds"),
+    file_refit = "on_change" # plausible options: "on_change" or "always"
   )
 
-
-
-
-
-
-
-
+# END  --------------------------------------------------------------------
