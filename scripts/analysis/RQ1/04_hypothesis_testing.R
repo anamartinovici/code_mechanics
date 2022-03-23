a4f7000e2681697644add93894a2f01b8fd90f27
Antonio Schettino <schettino@eur.nl>
2022-03-23 16:30:26 +0100

    add ±

--- scripts/analysis/RQ1/04_hypothesis_testing.R
+++ scripts/analysis/RQ1/04_hypothesis_testing.R
@@ -95,7 +95,7 @@ all_equivalence_test_N1_brms
 
 # Results: 95% of the posterior distribution 
 # of the N1 amplitude difference between manmade and natural scenes
-# is outside of a region of practical equivalence up until 0.09 µV.
+# is outside of a region of practical equivalence up until ±0.09 µV.
 # In other words, manmade scenes elicit an N1 whose amplitude is at most 
 # 0.09 µV larger than the N1 elicited by natural scenes.
 
